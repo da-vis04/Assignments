a39dcb256767e3e3cc3e8570afcecb5a26d8a7a9
da-vis04 <106776973+da-vis04@users.noreply.github.com>
2022-10-28 13:03:26 +0100

    Delete Week-5/Server directory

--- Week-5/Server/oath.js
+++
@@ -1,6 +0,0 @@
-const path = require("path")
-
-let filePath = path.join(__dirname, 'oath.js')
-let fileExtension = path.extname(filePath)
-let basename = path.basename(filePath)
-console.log(basename);
